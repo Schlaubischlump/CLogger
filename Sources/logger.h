95cb1df77fae35043fe453a8d9a1eb3e569412ea
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2022-03-21 20:17:18 +0100

    Initial version

---
+++ Sources/logger.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  
+//
+//  Created by David Klopp on 21.03.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Logger.
+FOUNDATION_EXPORT double LoggerVersionNumber;
+
+//! Project version string for Logger.
+FOUNDATION_EXPORT const unsigned char LoggerVersionString[];
